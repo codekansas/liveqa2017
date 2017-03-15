e5cfdaf1260983f9d54308dfddfda9073c59b7d1
qian6100 <weiqian.yan@emory.edu>
2017-03-15 19:10:17 -0400

    Shallow Rank class, with train and getCandidates method

--- liveqa/rank/ShallowRank.py
+++ liveqa/rank/ShallowRank.py
@@ -1,26 +1,29 @@
 from gensim import models,corpora,similarities
-
+import pickle
 class ShallowRank(object):
+    def __init__(self):
+        self.stoplist = set('for a of the and to in'.split())
 
-    def __init__(self,numberOfCandidates,query,candidates):
-        self.nc = numberOfCandidates
-        self.query = query
-        self.candidates = candidates
-
-    def shallowRank(self):
-        stoplist = set('for a of the and to in'.split())
-        documents = [[word for word in document.lower().split() if word not in stoplist] for document in self.candidates]
+    def train(self,corpus):
+        documents = [[word for word in document.lower().split() if word not in self.stoplist] for document in
+                     corpus]
         dictionary = corpora.Dictionary(documents)
-        query_vec = dictionary.doc2bow(self.query.lower().split())
+        pickle.dump(dictionary,open("dictionary.p","wb"))
+
+    def getCandidates(self,nc,query,candidates):
+        dictionary = pickle.load(open("dictionary.p","rb"))
+        query_vec = dictionary.doc2bow(query.lower().split())
+        documents = [[word for word in document.lower().split() if word not in self.stoplist] for document in candidates]
         corpus = [dictionary.doc2bow(doc) for doc in documents]
         lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
         vec_lsi = lsi[query_vec]
         index = similarities.MatrixSimilarity(lsi[corpus])
         sims = index[vec_lsi]
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
-        if self.nc <= len(sims):
-            selected = [sim[0] for sim in sims[0:self.nc]]
+        if nc <= len(sims):
+            selected = [sim[0] for sim in sims[0:nc]]
         else:
             selected = [sim[0] for sim in sims]
-        results = [self.candidates[n] for n in selected]
+        results = [candidates[n] for n in selected]
         return results
+
