b4233636a8b558d47691107a498401740903cb33
Benjamin Bolte <bkbolte18@gmail.com>
2017-03-17 02:02:23 -0400

    switched to attention model :fire:

--- liveqa/nn/model.py
+++ liveqa/nn/model.py
@@ -76,7 +76,11 @@ class ActivationWrapper(tf.contrib.rnn.RNNCell):
 
 
 class AttentionCellWrapper2D(tf.contrib.rnn.RNNCell):
-    """Basic attention cell wrapper, using 2D attention input."""
+    """Basic attention cell wrapper, using 2D attention input.
+
+    This isn't really an "attention" mechanism, more like a "peak" mechanism.
+    There were a number of issues
+    """
 
     def __init__(self, cell, attn_vec):
         if not isinstance(cell, tf.contrib.rnn.RNNCell):
@@ -103,138 +107,6 @@ class AttentionCellWrapper2D(tf.contrib.rnn.RNNCell):
     @property
     def output_size(self):
         return self._cell.state_size
-
-
-def noisy_decoder_fn_inferrence(output_fn, encoder_state, embeddings,
-                                start_of_sequence_id, end_of_sequence_id,
-                                maximum_length, num_decoder_symbols,
-                                temperature, dtype=dtypes.int32, name=None):
-    """ Simple decoder function for a sequence-to-sequence model used in the
-    `dynamic_rnn_decoder`.
-
-    The `simple_decoder_fn_inference` is a simple inference function for a
-    sequence-to-sequence model. It should be used when `dynamic_rnn_decoder` is
-    in the inference mode.
-
-    The `simple_decoder_fn_inference` is called with a set of the user arguments
-    and returns the `decoder_fn`, which can be passed to the
-    `dynamic_rnn_decoder`, such that
-
-    ```
-    dynamic_fn_inference = simple_decoder_fn_inference(...)
-    outputs_inference, state_inference = dynamic_rnn_decoder(
-        decoder_fn=dynamic_fn_inference, ...)
-    ```
-
-    Further usage can be found in the `kernel_tests/seq2seq_test.py`.
-
-    Args:
-        output_fn: An output function to project your `cell_output` onto class
-        logits.
-        encoder_state: The encoded state to initialize the
-        `dynamic_rnn_decoder`.
-        embeddings: The embeddings matrix used for the decoder sized
-        `[num_decoder_symbols, embedding_size]`.
-        start_of_sequence_id: The start of sequence ID in the decoder
-        embeddings.
-        end_of_sequence_id: The end of sequence ID in the decoder embeddings.
-        maximum_length: The maximum allowed of time steps to decode.
-        num_decoder_symbols: The number of classes to decode at each time step.
-        temperature: float or 0D Tensor, the temperature of the noise.
-        dtype: (default: `dtypes.int32`) The default data type to use when
-        handling integer objects.
-        name: (default: `None`) NameScope for the decoder function;
-            defaults to "simple_decoder_fn_inference"
-
-    Returns:
-        A decoder function with the required interface of `dynamic_rnn_decoder`
-        intended for inference.
-    """
-    with ops.name_scope(name, "simple_decoder_fn_inference",
-                        [output_fn, encoder_state, embeddings,
-                         start_of_sequence_id, end_of_sequence_id,
-                         maximum_length, num_decoder_symbols, dtype]):
-        start_of_sequence_id = ops.convert_to_tensor(start_of_sequence_id, dtype)
-        end_of_sequence_id = ops.convert_to_tensor(end_of_sequence_id, dtype)
-        maximum_length = ops.convert_to_tensor(maximum_length, dtype)
-        num_decoder_symbols = ops.convert_to_tensor(num_decoder_symbols, dtype)
-        encoder_info = nest.flatten(encoder_state)[0]
-        batch_size = encoder_info.get_shape()[0].value
-        if output_fn is None:
-            output_fn = lambda x: x
-        if batch_size is None:
-            batch_size = array_ops.shape(encoder_info)[0]
-
-    def decoder_fn(time, cell_state, cell_input, cell_output, context_state):
-        """ Decoder function used in the `dynamic_rnn_decoder` with the purpose
-        of inference.
-
-        The main difference between this decoder function and the `decoder_fn`
-        in `simple_decoder_fn_train` is how `next_cell_input` is calculated. In
-        this decoder function we calculate the next input by applying an argmax
-        across the feature dimension of the output from the decoder. This is a
-        greedy-search approach. (Bahdanau et al., 2014) & (Sutskever et al.,
-        2014) use beam-search instead.
-
-        Args:
-            time: positive integer constant reflecting the current timestep.
-            cell_state: state of RNNCell.
-            cell_input: input provided by `dynamic_rnn_decoder`.
-            cell_output: output of RNNCell.
-            context_state: context state provided by `dynamic_rnn_decoder`.
-
-        Returns:
-            A tuple (done, next state, next input, emit output, next context
-            state) where:
-
-            done: A boolean vector to indicate which sentences has reached a
-            `end_of_sequence_id`. This is used for early stopping by the
-            `dynamic_rnn_decoder`. When `time>=maximum_length` a boolean vector
-            with all elements as `true` is returned.
-
-            next state: `cell_state`, this decoder function does not modify the
-            given state.
-
-            next input: The embedding from argmax of the `cell_output` is used
-            as `next_input`.
-
-            emit output: If `output_fn is None` the supplied `cell_output` is
-            returned, else the `output_fn` is used to update the `cell_output`
-            before calculating `next_input` and returning `cell_output`.
-
-            next context state: `context_state`, this decoder function does not
-            modify the given context state. The context state could be modified
-            when applying e.g. beam search.
-    """
-        with ops.name_scope(name, "simple_decoder_fn_inference",
-                            [time, cell_state, cell_input, cell_output,
-                             context_state, temperature]):
-            if cell_input is not None:
-                raise ValueError("Expected cell_input to be None, but saw: %s" %
-                                 cell_input)
-            if cell_output is None:
-                next_input_id = array_ops.ones([batch_size,], dtype=dtype) * (
-                        start_of_sequence_id)
-                done = array_ops.zeros([batch_size,], dtype=dtypes.bool)
-                cell_state = encoder_state
-                cell_output = array_ops.zeros([num_decoder_symbols],
-                                              dtype=dtypes.float32)
-            else:
-                cell_output = output_fn(cell_output)
-                log_div = math_ops.log(nn_ops.softmax(cell_output))
-                exp_log_div = math_ops.exp(log_div / temperature)
-                renormalized = nn_ops.softmax(exp_log_div)
-                choices = random_ops.multinomial(renormalized, 1)
-                choices = array_ops.reshape(choices, (batch_size,))
-                next_input_id = math_ops.cast(choices, dtype=dtype)
-                done = math_ops.equal(next_input_id, end_of_sequence_id)
-            next_input = array_ops.gather(embeddings, next_input_id)
-            done = control_flow_ops.cond(math_ops.greater(time, maximum_length),
-                lambda: array_ops.ones([batch_size,], dtype=dtypes.bool),
-                lambda: done)
-            return (done, cell_state, next_input, cell_output, context_state)
-
-    return decoder_fn
 
 
 class QuestionGenerator(object):
@@ -348,16 +220,7 @@ class QuestionGenerator(object):
             return x
 
         with tf.variable_scope('encoder'):
-            # x = tf.expand_dims(x, axis=2)
-            # x = _add_conv(x, 1, 64, 5, 2)
-            # x = _add_conv(x, 1, 64, 5, 2)
-            # x = _add_conv(x, 1, 64, 5, 2)
-            # x = tf.squeeze(x, axis=2)
-
-            cells = [tf.contrib.rnn.GRUCell(rnn_size) for _ in range(num_rnn)]
-            cells = [tf.contrib.rnn.DropoutWrapper(
-                cell, output_keep_prob=0.7) for cell in cells]
-            cell = tf.contrib.rnn.MultiRNNCell(cells)
+            cell = tf.contrib.rnn.GRUCell(rnn_size)
             cell = tf.contrib.rnn.FusedRNNCellAdaptor(cell,
                                                       use_dynamic_rnn=True)
 
@@ -366,7 +229,7 @@ class QuestionGenerator(object):
                     tf.transpose(x, (1, 0, 2)),
                     dtype=tf.float32,
                     sequence_length=self.input_len_pl)
-                attn_vec_fw = tf.reduce_max(attn_vec_fw, axis=0)
+                # attn_vec_fw = tf.reduce_max(attn_vec_fw, axis=0)
 
             with tf.variable_scope('backward'):
                 cell_bw = tf.contrib.rnn.TimeReversedFusedRNN(cell)
@@ -374,14 +237,19 @@ class QuestionGenerator(object):
                     tf.transpose(x, (1, 0, 2)),
                     dtype=tf.float32,
                     sequence_length=self.input_len_pl)
-                attn_vec_bw = tf.reduce_max(attn_vec_bw, axis=0)
+                # attn_vec_bw = tf.reduce_max(attn_vec_bw, axis=0)
 
             attn_vec = tf.concat([attn_vec_fw, attn_vec_bw], axis=-1)
-            enc_states = tuple(tf.concat([f, b], axis=-1) for f, b in
-                               zip(enc_states_fw, enc_states_bw))
+            enc_states = tf.concat([enc_states_fw, enc_states_bw], axis=-1)
 
-        train_decoder_fn = tf.contrib.seq2seq.simple_decoder_fn_train(
-            encoder_state=enc_states)
+        attn_vec = tf.transpose(attn_vec, (1, 0, 2))
+
+        # Builds the attention component.
+        k, v, score_fn, construct_fn = tf.contrib.seq2seq.prepare_attention(
+            attn_vec, 'luong', rnn_size * 2)
+
+        train_decoder_fn = tf.contrib.seq2seq.attention_decoder_fn_train(
+            enc_states, k, v, score_fn, construct_fn)
 
         # Appends the start token to the first position of the target_pl.
         start_idx = tf.ones((tf.shape(self.target_pl)[0], 1), dtype=tf.int32)
@@ -390,19 +258,23 @@ class QuestionGenerator(object):
         seq_vals = tf.nn.embedding_lookup(emb, seq_vals)
 
         # Builds the RNN decoder training function.
-        cells = [tf.contrib.rnn.GRUCell(rnn_size * 2) for _ in range(num_rnn)]
-        cells = [tf.contrib.rnn.DropoutWrapper(
-            cell, output_keep_prob=0.7) for cell in cells]
-        cells = [AttentionCellWrapper2D(cell, attn_vec) for cell in cells]
-        cell = tf.contrib.rnn.MultiRNNCell(cells)
-        cell = tf.contrib.rnn.OutputProjectionWrapper(cell, self.num_classes)
-        # cell = ActivationWrapper(cell, 'softplus')
+        cell = tf.contrib.rnn.GRUCell(rnn_size * 2)
 
         x, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(
             cell=cell,
             decoder_fn=train_decoder_fn,
             inputs=seq_vals,
             sequence_length=self.target_len_pl)
+
+        output_W = self.get_weight('out_W', (rnn_size * 2, self.num_classes))
+        output_b = self.get_weight('out_b', (self.num_classes,), init='zero')
+
+        # Applies the output function time-wise.
+        x = tf.einsum('ijk,kl->ijl', x, output_W)
+        x = tf.nn.bias_add(x, output_b)
+
+        # Function for decoding layer on.
+        output_fn = lambda x: tf.matmul(x, output_W) + output_b
 
         # Masks all the values beyond the answer.
         weights = tf.sequence_mask(self.target_len_pl, dtype=tf.float32)
@@ -426,27 +298,23 @@ class QuestionGenerator(object):
         capped = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
         self.train_op = optimizer.apply_gradients(capped)
 
-        # infer_decoder_fn = noisy_decoder_fn_inferrence(
-        #     output_fn=None,
-        #     encoder_state=enc_states,
-        #     embeddings=emb,
-        #     start_of_sequence_id=yahoo.START_IDX,
-        #     end_of_sequence_id=yahoo.END_IDX,
-        #     maximum_length=self.output_len,
-        #     num_decoder_symbols=self.num_classes,
-        #     temperature=self._temp_pl)
+        # Reuse the RNN from earlier.
+        tf.get_variable_scope().reuse_variables()
 
-        infer_decoder_fn = tf.contrib.seq2seq.simple_decoder_fn_inference(
-            output_fn=None,
+        # Builds the attention decoder.
+        infer_decoder_fn = tf.contrib.seq2seq.attention_decoder_fn_inference(
+            output_fn=output_fn,
             encoder_state=enc_states,
+            attention_keys=k,
+            attention_values=v,
+            attention_score_fn=score_fn,
+            attention_construct_fn=construct_fn,
             embeddings=emb,
             start_of_sequence_id=yahoo.START_IDX,
             end_of_sequence_id=yahoo.END_IDX,
             maximum_length=self.output_len,
             num_decoder_symbols=self.num_classes)
 
-        # Reuse the RNN from earlier.
-        tf.get_variable_scope().reuse_variables()
         self.inferred_question, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(
             cell=cell,
             decoder_fn=infer_decoder_fn)
